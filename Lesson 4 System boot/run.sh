ddaf4931ea4a2de62e88b441bf27587b59a97a83
alvic <alvic@alvic-nuc.home>
2018-08-08 13:48:56 +0300

    Lesson 4 added files

---
+++ Lesson 4 System boot/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+exec 0<>/dev/console 1<>/dev/console 2<>/dev/console
+cat <<'msgend'
+
+          (__)             (__)             (__)             (__)
+          (oo)             (oo)             (oo)             (oo)
+   /-------\/       /-------\/       /-------\/       /-------\/
+  / |     ||       / |     ||       / |     ||       / |     ||
+ *  ||----||      *  ||W---||      *  ||w---||      *  ||V---||
+    ^^    ^^         ^^    ^^         ^^    ^^         ^^    ^^
+      Cow           Cow laden        Same cow      Nancy Reagan-type
+                    with milk     after milking      cow with milk
+
+msgend
+sleep 10
+echo " continuing...."
