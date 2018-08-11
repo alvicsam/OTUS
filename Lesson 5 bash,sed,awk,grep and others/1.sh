ebe95d989a61ad43da200624a4bbfd9fb98084eb
alvicsam <41779041+alvicsam@users.noreply.github.com>
2018-08-12 00:15:36 +0300

    Update 1.sh

--- Lesson 5 bash,sed,awk,grep and others/1.sh
+++ Lesson 5 bash,sed,awk,grep and others/1.sh
@@ -4,7 +4,7 @@ while true; do
     nginx_status=$(systemctl is-active nginx)
     echo "nginx is in $nginx_status state"
     if [ $nginx_status != "active" ] ;then
-	echo something is terribly wrong!
+	echo "something is terribly wrong!"
 	sudo systemctl start nginx
 	nginx_status=$(systemctl is-active nginx)
 	echo "But now everything is ok and nginx is in $nginx_status state"
